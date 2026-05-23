e3cddfc18a50d64f0b83d0f82fe67283e01c8a34
IblazeVape <vapeumbrellamanchester@gmail.com>
2026-05-23 11:47:49 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,16 @@
-import React from "react";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import { AppSidebar } from "@/components/app-sidebar";
-import { SiteHeader } from "@/components/site-header";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
-}: {
+  children
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body>
-        <SidebarProvider
-          style={
-            {
-              "--sidebar-width": "288px",
-              "--header-height": "48px",
-            } as React.CSSProperties
-          }
-        >
-          <AppSidebar variant="inset" />
-          <SidebarInset>
-            <SiteHeader />
-            <div className="flex flex-1 flex-col">
-              <div className="@container/main flex flex-1 flex-col gap-2">
-                <div className="flex flex-col gap-4 md:gap-6 p-4 lg:p-6">
-                  {children}
-                </div>
-              </div>
-            </div>
-          </SidebarInset>
-        </SidebarProvider>
-      </body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
